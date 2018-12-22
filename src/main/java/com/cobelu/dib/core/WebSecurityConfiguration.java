8761f1bed6168166ddee4a4b45a0717595e86216
Connor Luckett <cluckett15@austincollege.edu>
2018-12-22 15:15:23 -0600

    temporary warning suppression until RDS migration

--- src/main/java/com/cobelu/dib/core/WebSecurityConfiguration.java
+++ src/main/java/com/cobelu/dib/core/WebSecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
