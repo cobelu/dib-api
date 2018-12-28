67507bc55a1cde844a27ac5495a048f8a5502781
Connor Luckett <cluckett15@austincollege.edu>
2018-12-28 10:24:51 -0600

    login form

--- src/main/java/com/cobelu/dib/core/WebSecurityConfiguration.java
+++ src/main/java/com/cobelu/dib/core/WebSecurityConfiguration.java
@@ -21,18 +21,22 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		/*
-		 * TODO: Swap for not using H2
+		 * Use this when not using H2
 		 */
-		// auth.userDetailsService(userDetailsService).passwordEncoder(User.PASSWORD_ENCODER);
+//		auth.userDetailsService(userDetailsService).passwordEncoder(User.PASSWORD_ENCODER);
 
+		/*
+		 * Use this if problems with H2 testing login
+		 */
 		auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance()).withUser("test")
 				.password("password").roles("USER");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO: REMOVE AND CSRF DISABLE STATEMENT WHEN DONE WITH TESTING
-		http.authorizeRequests().anyRequest().authenticated().and().httpBasic().and().csrf().disable();
+		// REMOVE AND CSRF DISABLE STATEMENT WHEN DONE WITH TESTING
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin().permitAll();
+//		http.authorizeRequests().anyRequest().authenticated().and().httpBasic()and().csrf().disable()
 	}
 
 }
