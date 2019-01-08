5998393a24dfb4a5e2768642d8f4a761569f86be
Connor Luckett <cluckett15@austincollege.edu>
2019-01-08 15:56:43 -0600

    removal of unused imports

--- src/main/java/com/cobelu/dib/core/WebSecurityConfiguration.java
+++ src/main/java/com/cobelu/dib/core/WebSecurityConfiguration.java
@@ -1,20 +1,12 @@
 package com.cobelu.dib.core;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @SuppressWarnings("deprecation")
 @Configuration
