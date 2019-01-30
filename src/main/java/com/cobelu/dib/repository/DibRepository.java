79fb0d7fdaec33d90290c97be659fd131e2fea33
cobelu <cobelu@192.168.1.100>
2019-01-29 18:26:51 -0600

    Find dibs by user's username

--- src/main/java/com/cobelu/dib/repository/DibRepository.java
+++ src/main/java/com/cobelu/dib/repository/DibRepository.java
@@ -1,6 +1,10 @@
 package com.cobelu.dib.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.cobelu.dib.model.Dib;
 
@@ -11,4 +15,7 @@ import com.cobelu.dib.model.Dib;
  */
 public interface DibRepository extends PagingAndSortingRepository<Dib, Long> {
 
+	@RestResource(rel = "with-user-username", path = "withUserUsername")
+	Page<Dib> findByUser_Username(@Param("userUsername") String username, Pageable page);
+	
 }
